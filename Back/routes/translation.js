74f1f35f2479ffcec15ed73a620e5a5a6ea97832
Daniel0205 <daniel.alejandro.diaz@correounivalle.edu.co>
2020-12-19 10:38:53 -0500

    memory added

--- Back/routes/translation.js
+++ Back/routes/translation.js
@@ -7,31 +7,68 @@ const headers = {
     'Content-Type': 'application/json',
     'Authorization': process.env.NODE_ENV_AUTH,
 }
- 
+
+
+var translatedLyrics = {}
+
+
+function searchAuthor(author){
+    if(translatedLyrics[author]) return true
+    else return false
+}
+
+
+function searchSong(author,song){
+
+    if(searchAuthor(author)){
+        if(translatedLyrics[author][song]) return true
+        else return false
+    }
+    else return false
+}
+
+
+function saveNewSong(translation, author, song){
+    
+    if(!searchAuthor(author))translatedLyrics[author] = {}
+    translatedLyrics[author][song] = translation
+    
+}
 
 router.post('/translate', (req, res) => {
 
-    if(req.body.lyrics!==""){
-        axios.post('https://api-b2b.backenster.com/b1/api/v3/translate',{
-            from: "en_GB",
-            to: "es_CO",
-            data: req.body.lyrics,
-            platform: "api"
-        },{
-            headers: headers
-        })
-        .then(result => {
-            
-            return res.json({spanishLyrics:result.data.targetTransliteration})
-        })
-        .catch((error) => {
-            return res.json({spanishLyrics:"error"})
-        })
+    var author = req.body.author;
+    var song = req.body.song;
+
+    if(searchSong(author,song)){
+        return res.json({spanishLyrics:translatedLyrics[author][song]})
     }
     else{
-        return res.json({spanishLyrics:""})
+
+        if(req.body.lyrics!==""){
+            axios.post('https://api-b2b.backenster.com/b1/api/v3/translate',{
+                from: "en_GB",
+                to: "es_CO",
+                data: req.body.lyrics,
+                platform: "api"
+            },{
+                headers: headers
+            })
+            .then(result => {
+                saveNewSong(result.data.targetTransliteration,author,song)
+                
+                return res.json({spanishLyrics:result.data.targetTransliteration})
+            })
+            .catch((error) => {
+                return res.json({spanishLyrics:"error"})
+            })
+        }
+        else{
+            return res.json({spanishLyrics:""})
+        }
+
     }
-    
+
 
 });
 
